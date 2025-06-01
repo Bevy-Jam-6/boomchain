9ef55544052f754010d475fefa4a528f5ef7135e
Jan Hohenheim <jan@hohenheim.ch>
2025-06-01 18:46:45 +0200

    Run cargo fmt

--- src/gameplay/npc/navigation.rs
+++ src/gameplay/npc/navigation.rs
@@ -15,8 +15,7 @@ use bevy_simple_subsecond_system::hot;
 use bevy_tnua::prelude::*;
 
 use crate::{
-    PrePhysicsAppSystems,
-    gameplay::player::navmesh_position::LastValidPlayerNavmeshPosition,
+    PrePhysicsAppSystems, gameplay::player::navmesh_position::LastValidPlayerNavmeshPosition,
     screens::Screen,
 };
 
