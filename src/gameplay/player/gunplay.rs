8e087d86a116ab6ac2b48a7c29da6c78bacc1885
Mathias Fischler <mafi@fog.im>
2025-06-01 20:10:03 +0200

    Print hits using cast_ray and ray_hits

--- src/gameplay/player/gunplay.rs
+++ src/gameplay/player/gunplay.rs
@@ -1,19 +1,17 @@
-use std::time::Duration;
-
+use super::{animation::PlayerAnimations, camera::PlayerCamera, default_input::Shoot};
+use crate::gameplay::{animation::AnimationPlayers, crosshair::CrosshairState};
+use avian3d::prelude::*;
 use bevy::prelude::*;
 use bevy_enhanced_input::events::Started;
-
-use crate::gameplay::{animation::AnimationPlayers, crosshair::CrosshairState};
-
-use super::{animation::PlayerAnimations, default_input::Shoot};
+use std::time::Duration;
 
 pub(super) fn plugin(app: &mut App) {
     app.add_observer(shooting);
+    app.add_observer(print_hits);
 }
 
 fn shooting(
     _trigger: Trigger<Started<Shoot>>,
-    mut commands: Commands,
     mut query: Query<&AnimationPlayers>,
     mut q_animation: Query<(
         &PlayerAnimations,
@@ -33,5 +31,39 @@ fn shooting(
                 );
             }
         }
+    }
+}
+
+fn print_hits(
+    _trigger: Trigger<Started<Shoot>>,
+    spatial_query: SpatialQuery,
+    player_camera_parent: Single<&Transform, With<PlayerCamera>>,
+    name: Query<NameOrEntity>,
+) {
+    info!("pew!");
+
+    // Ray origin and direction
+    let origin = player_camera_parent.translation;
+    let direction = player_camera_parent.forward();
+
+    // Configuration for the ray cast
+    let max_distance = 100.0;
+    let solid = true;
+    let filter = SpatialQueryFilter::default();
+
+    // Cast ray and print first hit
+    if let Some(first_hit) = spatial_query.cast_ray(origin, direction, max_distance, solid, &filter)
+    {
+        let name = name.get(first_hit.entity).unwrap();
+        info!("First hit: {name}");
+    }
+
+    // Cast ray and get up to 20 hits
+    let hits = spatial_query.ray_hits(origin, direction, max_distance, 20, solid, &filter);
+
+    // Print hits
+    for hit in hits.iter() {
+        let name = name.get(hit.entity).unwrap();
+        info!("Hit: {name}");
     }
 }
