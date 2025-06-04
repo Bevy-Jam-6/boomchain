f4ac0fce7858bff9d58f571175ffb41d42349d86
Jan Hohenheim <jan@hohenheim.ch>
2025-06-04 02:34:48 +0200

    Implement gibbing

--- src/gameplay/npc/lifecycle.rs
+++ src/gameplay/npc/lifecycle.rs
@@ -1,6 +1,11 @@
+use avian3d::prelude::{ColliderConstructor, ColliderConstructorHierarchy, RigidBody};
 use bevy::prelude::*;
+use bevy_shuffle_bag::ShuffleBag;
 
-use crate::gameplay::{health::OnDeath, npc::Npc};
+use crate::gameplay::{
+    health::OnDeath,
+    npc::{Npc, assets::NpcAssets},
+};
 
 pub(super) fn plugin(app: &mut App) {
     app.add_observer(on_enemy_death);
@@ -8,12 +13,44 @@ pub(super) fn plugin(app: &mut App) {
 
 fn on_enemy_death(
     trigger: Trigger<OnDeath>,
-    enemies: Query<(), With<Npc>>,
+    enemies: Query<&Transform, With<Npc>>,
+    npc_assets: Res<NpcAssets>,
     mut commands: Commands,
 ) {
     let entity = trigger.target();
-    if !enemies.contains(entity) {
+    let Some(transform) = enemies.get(entity).ok() else {
         return;
+    };
+    let mut rng = rand::thread_rng();
+    let mut gibs = ShuffleBag::try_new(
+        [
+            &npc_assets.gib_head,
+            &npc_assets.gib_torso,
+            &npc_assets.gib_arm_1,
+            &npc_assets.gib_arm_2,
+            &npc_assets.gib_arm_1,
+            &npc_assets.gib_arm_2,
+            &npc_assets.gib_leg,
+            &npc_assets.gib_leg,
+            &npc_assets.gib_foot,
+            &npc_assets.gib_foot,
+            &npc_assets.gib_pelvis,
+        ],
+        &mut rng,
+    )
+    .unwrap();
+    let num_gibs = 5;
+    for _ in 0..num_gibs {
+        let gib = *gibs.pick(&mut rng);
+        let offset_radius = 0.5;
+        let offset = Sphere::new(offset_radius).sample_interior(&mut rng);
+        let position = transform.translation + offset;
+        commands.spawn((
+            SceneRoot(gib.clone()),
+            Transform::from_translation(position),
+            RigidBody::Dynamic,
+            ColliderConstructorHierarchy::new(ColliderConstructor::ConvexHullFromMesh),
+        ));
     }
     commands.entity(entity).try_despawn();
 }
