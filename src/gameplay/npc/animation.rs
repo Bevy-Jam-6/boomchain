d7567732d0dc932aa60bfb750ab50eda12a2e568
Jan Hohenheim <jan@hohenheim.ch>
2025-07-01 17:52:03 +0200

    Run cargo fmt

--- src/gameplay/npc/animation.rs
+++ src/gameplay/npc/animation.rs
@@ -119,10 +119,10 @@ fn play_animations(
                     if let NpcAnimationState::Walking(speed) = state
                         && let Some((_index, playing_animation)) =
                             anim_player.playing_animations_mut().next()
-                        {
-                            let anim_speed = (speed / 5.0).max(0.2);
-                            playing_animation.set_speed(anim_speed);
-                        }
+                    {
+                        let anim_speed = (speed / 5.0).max(0.2);
+                        playing_animation.set_speed(anim_speed);
+                    }
                 }
                 TnuaAnimatingStateDirective::Alter {
                     // We don't need the old state here, but it's available for transition
